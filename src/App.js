fbaea369b1897495a11937bd0bce12091f718c17
Shayan Biswas <123059890+sbrb@users.noreply.github.com>
2023-06-02 00:56:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import NavBar from './components/NavBar';
+import { createTheme,ThemeProvider } from '@mui/material/styles';
+import HeroSection from './components/HeroSection';
+import { Container } from '@mui/material';
+import CakeSection from './components/CakeSection';
+import ContactUs from './components/ContactUs';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#864313',
+    },
+  },
+});
+
+function App() {
+  return (
+   <ThemeProvider theme={theme}>
+       <NavBar />
+       <Container>
+          <HeroSection /> 
+          <CakeSection />
+          <ContactUs />
+       </Container>
+   </ThemeProvider>
+  );
+}
+
+export default App;
